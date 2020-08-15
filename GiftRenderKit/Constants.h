58a1efb99a77b94dd476ecb1eb5825aee4777b5b
myvideoyun <developers@myvideoyun.com>
2020-08-15 17:29:24 +0800

    Add company prefix to avoid conflict

--- GiftRenderKit/Constants.h
+++ GiftRenderKit/Constants.h
@@ -1,10 +1,8 @@
 #ifndef TinyGiftConstants_h
 #define TinyGiftConstants_h
 
-extern NSString *const RenderNotification;
-extern NSString *const RenderUserInfo;
-extern NSString *const AuthNotify;
-extern NSString *const AuthUserInfo;
+extern NSString *const MVYGiftRenderNotification;
+extern NSString *const MVYGiftRenderUserInfo;
 typedef NS_ENUM(NSUInteger, AuthResult) {
     AuthSuccess,
     AuthFail,
--- GiftRenderKit/Constants.m
+++ GiftRenderKit/Constants.m
@@ -1,6 +1,4 @@
 #import <Foundation/Foundation.h>
 
-NSString *const RenderNotification = @"GiftRenderNotification";
-NSString *const RenderUserInfo = @"GiftRenderResult";
-NSString *const AuthNotify = @"LicenseNotification";
-NSString *const AuthUserInfo = @"LicenseResult";
+NSString *const MVYGiftRenderNotification = @"GiftRenderNotification";
+NSString *const MVYGiftRenderUserInfo = @"GiftRenderResult";
