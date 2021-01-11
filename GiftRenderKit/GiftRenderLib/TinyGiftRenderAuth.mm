a123930ebae4b65a940af00606308d2994cf6426
myvideoyun <developers@myvideoyun.com>
2021-01-11 14:47:12 +0800

    Support another authenticate mode

--- GiftRenderKit/GiftRenderLib/TinyGiftRenderAuth.mm
+++ GiftRenderKit/GiftRenderLib/TinyGiftRenderAuth.mm
@@ -11,4 +11,8 @@
     return renderer_auth(std::string([bundleIdentifier UTF8String]), std::string(appKey.UTF8String), length);
 }
 
++ (int)requestAuthEx:(NSString *)license license_length:(int)license_length key_str:(NSString *)appKey auth_length:(int)length {
+    NSString *bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
+    return renderer_auth_ex(std::string([bundleIdentifier UTF8String]), std::string(license.UTF8String), license_length, std::string(appKey.UTF8String), length);
+}
 @end
--- GiftRenderKit/MVYSDKAuthTool.m
+++ GiftRenderKit/MVYSDKAuthTool.m
@@ -7,4 +7,7 @@
     return [TinyGiftRenderAuth requestAuth:appKey auth_length:length];
 }
 
++ (int)requestAuthEx:(NSString *)license LicenseLength:(int)license_length Key:(NSString *)appKey KeyLength:(int)length{
+    return [TinyGiftRenderAuth requestAuthEx:license LicenseLength:license_length Key:appKey KeyLength:length];
+}
 @end
